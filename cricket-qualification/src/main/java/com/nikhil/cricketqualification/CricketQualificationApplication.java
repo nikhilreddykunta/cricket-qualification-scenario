dec368f42e01c9f53cf384667ad8b1476d2b8de0
nikhilreddykunta <kunta.nike.n@gmail.com>
2024-02-28 23:21:51 +0530

    added backend server

---
+++ cricket-qualification/src/main/java/com/nikhil/cricketqualification/CricketQualificationApplication.java
@@ -0,0 +1,13 @@
+package com.nikhil.cricketqualification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketQualificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketQualificationApplication.class, args);
+	}
+
+}
---
+++ cricket-qualification/src/test/java/com/nikhil/cricketqualification/CricketQualificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhil.cricketqualification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricketQualificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
